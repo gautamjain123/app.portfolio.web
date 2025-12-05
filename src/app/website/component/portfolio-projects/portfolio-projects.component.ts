10dd325f1877ce07a7fb30b3f20ae9bcc102bd55
gautam jain <gautam.jain@netoyed.com>
2025-12-05 17:42:32 +0530

    portfolio webpage

--- src/app/website/component/portfolio-projects/portfolio-projects.component.ts
+++ src/app/website/component/portfolio-projects/portfolio-projects.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio-projects.component.html',
   styleUrl: './portfolio-projects.component.scss'
 })
 export class PortfolioProjectsComponent {
-
+  projects = [
+    {
+      title: 'Immersive 3D Portfolio',
+      desc: 'A cinematic personal experience with interactive motion UI built using Angular + WebGL layers.',
+      tech: ['Angular', 'Three.js', 'GSAP'],
+    },
+    {
+      title: 'Job Matching Dashboard',
+      desc: 'Smart filtering, job pipelines, resume automation, employer dashboards & modern analytics.',
+      tech: ['Angular', 'Bootstrap', 'REST APIs'],
+    },
+    {
+      title: 'UI Motion Playground',
+      desc: 'A collection of futuristic parallax & motion UI experiments showcasing interface expression.',
+      tech: ['GSAP', 'UX Motion', 'WebGL'],
+    }
+  ];
 }
--- src/app/website/pages/home-layout/landing-page/landing-page.component.ts
+++ src/app/website/pages/home-layout/landing-page/landing-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { PortfolioHeroComponent } from "../../../component/portfolio-hero/portfolio-hero.component";
 import { PortfolioAboutComponent } from "../../../component/portfolio-about/portfolio-about.component";
 import { PortfolioSkillsComponent } from "../../../component/portfolio-skills/portfolio-skills.component";
+import { PortfolioProjectsComponent } from "../../../component/portfolio-projects/portfolio-projects.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [PortfolioHeroComponent, PortfolioAboutComponent, PortfolioSkillsComponent],
+  imports: [PortfolioHeroComponent, PortfolioAboutComponent, PortfolioSkillsComponent, PortfolioProjectsComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
